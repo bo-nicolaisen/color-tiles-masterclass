1b6cda503adc6ec68895650c9a53ae81fe6a1089
bo-nicolaisen <bni@techcollege.dk>
2024-04-29 22:33:16 +0200

    Update site.js
    js

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,1 +1,46 @@
 // write cool JS hwere!!
+
+const baseColors = [ 
+    '#9e0142',
+    '#d53e4f',
+    '#f46d43',
+    '#fdae61',
+    '#fee08b',
+    '#abdda4',
+    '#66c2a5',
+    '#3288bd',
+    '#5e4fa2'
+  ];
+
+
+
+
+  createTiles()
+
+
+
+
+function createTiles(){
+
+    const myAppElement=document.getElementById("app");
+
+    const colorHeadline = document.createElement('h1');
+    colorHeadline.innerText = 'Color Palette';
+    myAppElement.appendChild(colorHeadline);
+
+
+    const myColorElement = document.createElement('section');
+    myColorElement.id="colors"
+   
+    myAppElement.appendChild(myColorElement);
+    
+    baseColors.forEach(color => {
+        const colorBox = document.createElement('div');
+        colorBox.classList.add('color-box');
+        colorBox.style.backgroundColor = color;
+        colorBox.textContent = color;
+        myColorElement.appendChild(colorBox);
+      });
+}
+
+// write cool JS hwere!!
